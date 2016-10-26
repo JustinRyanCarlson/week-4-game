f30feb2e4f2ff7cb88ae4743068f94246a4d82de
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-10-25 22:41:50 -0500

    adds styles to style.css, adds IDs to the hero's divs on index.html, adds on click events to game.js to move the enemies to the enemies available to attack section and from there gives the play the ability to add a enemy to the defender section

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,16 @@ var lukeHero = false;
 var obiHero = false;
 var sidiousHero = false;
 var maulHero = false;
+var lukeDefender = false;
+var obiDefender = false;
+var sidiousDefender = false;
+var maulDefender = false;
+var lukeStats = { hp: 100, dmg: '', counter: 5 };
+var obiStats = { hp: 120, dmg: 8, counter: '' };
+var sidiousStats = { hp: 150, dmg: '', counter: 20 };
+var maulStats = { hp: 180, dmg: '', counter: 25 }
+
+
 
 
 $('#Luke').click(function() {
@@ -12,23 +22,173 @@ $('#Luke').click(function() {
     $('#your-hero').html('Your Hero');
     $('#enemy1').html('<h5 class="text-center">Obi-Wan Kenobi</h5>' +
         '<img class="text-center" src="assets/images/obi-wan.jpg" alt="Obi-Wan Kenobi">' +
-        '<h5 class="text-center">120</h5>').addClass('enemy-char-boxes');
+        '<h5 class="text-center" id="obi-hp">120</h5>').addClass('enemy-char-boxes');
     $('#enemy2').html('<h5 class="text-center">Darth Sidious</h5>' +
         '<img class="text-center" src="assets/images/darth-sidious.png" alt="Darth Sidious">' +
-        '<h5 class="text-center">150</h5>').addClass('enemy-char-boxes');
+        '<h5 class="text-center" id="sidious-hp">150</h5>').addClass('enemy-char-boxes');
     $('#enemy3').html('<h5 class="text-center">Darth Maul</h5>' +
         '<img class="text-center" src="assets/images/darth-maul.jpg" alt="Darth Maul">' +
-        '<h5 class="text-center">180</h5>').addClass('enemy-char-boxes');
+        '<h5 class="text-center" id="maul-hp">180</h5>').addClass('enemy-char-boxes');
 })
+
+
 
 $('#Obi').click(function() {
     obiHero = true;
+    $('#Luke').removeClass('char-boxes').html('');
+    $('#Sidious').removeClass('char-boxes').html('');
+    $('#Maul').removeClass('char-boxes').html('');
+    $('#your-hero').html('Your Hero');
+    $('#enemy1').html('<h5 class="text-center">Luke Skywalker</h5>' +
+        '<img class="text-center" src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">' +
+        '<h5 class="text-center" id="luke-hp">100</h5>').addClass('enemy-char-boxes');
+    $('#enemy2').html('<h5 class="text-center">Darth Sidious</h5>' +
+        '<img class="text-center" src="assets/images/darth-sidious.png" alt="Darth Sidious">' +
+        '<h5 class="text-center" id="sidious-hp">150</h5>').addClass('enemy-char-boxes');
+    $('#enemy3').html('<h5 class="text-center">Darth Maul</h5>' +
+        '<img class="text-center" src="assets/images/darth-maul.jpg" alt="Darth Maul">' +
+        '<h5 class="text-center" id="maul-hp">180</h5>').addClass('enemy-char-boxes');
 })
+
+
 
 $('#Sidious').click(function() {
     sidiousHero = true;
+    $('#Obi').removeClass('char-boxes').html('');
+    $('#Luke').removeClass('char-boxes').html('');
+    $('#Maul').removeClass('char-boxes').html('');
+    $('#your-hero').html('Your Hero');
+    $('#enemy1').html('<h5 class="text-center">Luke Skywalker</h5>' +
+        '<img class="text-center" src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">' +
+        '<h5 class="text-center" id="luke-hp">100</h5>').addClass('enemy-char-boxes');
+    $('#enemy2').html('<h5 class="text-center">Obi-Wan Kenobi</h5>' +
+        '<img class="text-center" src="assets/images/obi-wan.jpg" alt="Obi-Wan Kenobi">' +
+        '<h5 class="text-center" id="obi-hp">120</h5>').addClass('enemy-char-boxes');
+    $('#enemy3').html('<h5 class="text-center">Darth Maul</h5>' +
+        '<img class="text-center" src="assets/images/darth-maul.jpg" alt="Darth Maul">' +
+        '<h5 class="text-center" id="maul-hp">180</h5>').addClass('enemy-char-boxes');
 })
+
+
 
 $('#Maul').click(function() {
     maulHero = true;
+    $('#Obi').removeClass('char-boxes').html('');
+    $('#Sidious').removeClass('char-boxes').html('');
+    $('#Luke').removeClass('char-boxes').html('');
+    $('#your-hero').html('Your Hero');
+    $('#enemy1').html('<h5 class="text-center">Luke Skywalker</h5>' +
+        '<img class="text-center" src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">' +
+        '<h5 class="text-center" id="luke-hp">100</h5>').addClass('enemy-char-boxes');
+    $('#enemy2').html('<h5 class="text-center">Obi-Wan Kenobi</h5>' +
+        '<img class="text-center" src="assets/images/obi-wan.jpg" alt="Obi-Wan Kenobi">' +
+        '<h5 class="text-center" id="obi-hp">120</h5>').addClass('enemy-char-boxes');
+    $('#enemy3').html('<h5 class="text-center">Darth Sidious</h5>' +
+        '<img class="text-center" src="assets/images/darth-sidious.png" alt="Darth Sidious">' +
+        '<h5 class="text-center" id="sidious-hp">150</h5>').addClass('enemy-char-boxes');
+})
+
+
+
+
+
+
+$('#enemy1').click(function() {
+    if (lukeDefender === false && obiDefender === false &&
+        sidiousDefender === false && maulDefender === false) {
+
+        if (lukeHero) {
+            $('#enemy1').removeClass('enemy-char-boxes').html('');
+            obiDefender = true;
+            $('#current-defender').html('<h5 class="text-center">Obi-Wan Kenobi</h5>' +
+                '<img class="text-center" src="assets/images/obi-wan.jpg" alt="Obi-Wan Kenobi">' +
+                '<h5 class="text-center" id="obi-hp">120</h5>').addClass('defender-char-boxes');
+        } else {
+            $('#enemy1').removeClass('enemy-char-boxes').html('');
+            lukeDefender = true;
+            $('#current-defender').html('<h5 class="text-center">Luke Skywalker</h5>' +
+                '<img class="text-center" src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">' +
+                '<h5 class="text-center" id="luke-hp">100</h5>').addClass('defender-char-boxes');
+        }
+    }
+})
+
+$('#enemy2').click(function() {
+    if (lukeDefender === false && obiDefender === false &&
+        sidiousDefender === false && maulDefender === false) {
+
+        if (lukeHero || obiHero) {
+            $('#enemy2').removeClass('enemy-char-boxes').html('');
+            sidiousDefender = true;
+            $('#current-defender').html('<h5 class="text-center">Darth Sidious</h5>' +
+                '<img class="text-center" src="assets/images/darth-sidious.png" alt="Darth Sidious">' +
+                '<h5 class="text-center" id="sidious-hp">150</h5>').addClass('defender-char-boxes');
+        } else if (sidiousHero || maulHero) {
+            $('#enemy2').removeClass('enemy-char-boxes').html('');
+            obiDefender = true;
+            $('#current-defender').html('<h5 class="text-center">Obi-Wan Kenobi</h5>' +
+                '<img class="text-center" src="assets/images/obi-wan.jpg" alt="Obi-Wan Kenobi">' +
+                '<h5 class="text-center" id="obi-hp">120</h5>').addClass('defender-char-boxes');
+        }
+    }
+})
+
+$('#enemy3').click(function() {
+    if (lukeDefender === false && obiDefender === false &&
+        sidiousDefender === false && maulDefender === false) {
+
+        if (lukeHero || obiHero || sidiousHero) {
+            $('#enemy3').removeClass('enemy-char-boxes').html('');
+            maulDefender = true;
+            $('#current-defender').html('<h5 class="text-center">Darth Maul</h5>' +
+                '<img class="text-center" src="assets/images/darth-maul.jpg" alt="Darth Maul">' +
+                '<h5 class="text-center" id="maul-hp">180</h5>').addClass('defender-char-boxes');
+        } else if (maulHero) {
+            $('#enemy3').removeClass('enemy-char-boxes').html('');
+            sidiousDefender = true;
+            $('#current-defender').html('<h5 class="text-center">Darth Sidious</h5>' +
+                '<img class="text-center" src="assets/images/darth-sidious.png" alt="Darth Sidious">' +
+                '<h5 class="text-center" id="sidious-hp">150</h5>').addClass('defender-char-boxes');
+        }
+    }
+})
+
+
+
+
+
+
+$('#attack-button').click(function() {
+    $('#status').addClass('status-padding');
+    if (lukeHero === false && obiHero === false &&
+        sidiousHero === false && maulHero === false) {
+        $('#status').html('You must choose a hero first!');
+    } else if (lukeDefender === false && obiDefender === false &&
+        sidiousDefender === false && maulDefender === false) {
+        $('#status').html('You must select a defender to attack!')
+    } else if (lukeHero === true && obiDefender === true) {
+
+    } else if (lukeHero === true && sidiousDefender === true) {
+
+    } else if (lukeHero === true && maulDefender === true) {
+
+    } else if (obiHero === true && lukeDefender === true) {
+
+    } else if (obiHero === true && sidiousDefender === true) {
+
+    } else if (obiHero === true && maulDefender === true) {
+
+    } else if (sidiousHero === true && lukeDefender === true) {
+
+    } else if (sidiousHero === true && obiDefender === true) {
+
+    } else if (sidiousHero === true && maulDefender === true) {
+
+    } else if (maulHero === true && lukeDefender === true) {
+
+    } else if (maulHero === true && obiDefender === true) {
+
+    } else if (maulHero === true && sidiousDefender === true) {
+
+    }
 })
